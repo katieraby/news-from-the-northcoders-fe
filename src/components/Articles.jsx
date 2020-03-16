8323ca3dc69460424414b97f710de9d796327fed
Katie Raby <katie.raby@icloud.com>
2020-03-16 14:59:25 +0000

    Harded data into articles state and added conditionals to render the correct data to ArticleList

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,14 +1,99 @@
 import React, { Component } from "react";
+import ArticleList from "./ArticleList";
 
 class Articles extends Component {
-  state = { articleData: [] };
+  state = {
+    articleData: [
+      {
+        article_id: 33,
+        title: "Seafood substitutions are increasing",
+        body:
+          "'SEAFOOD fraud is a serious global problem', begins a recent report from Oceana, an NGO. Reviewin...",
+        votes: 0,
+        topic: "cooking",
+        author: "weegembump",
+        created_at: "2018-05-30T15:59:13.341Z",
+        comment_count: "6"
+      },
+      {
+        article_id: 28,
+        title: "High Altitude Cooking",
+        body:
+          "Most backpacking trails vary only a few thousand feet elevation. However, many trails can be foun...",
+        votes: 0,
+        topic: "cooking",
+        author: "happyamy2016",
+        created_at: "2018-05-27T03:32:28.514Z",
+        comment_count: "5"
+      },
+      {
+        article_id: 30,
+        title:
+          "Twice-Baked Butternut Squash Is the Thanksgiving Side Dish of Your Dreams",
+        body:
+          "What if, for once, your Thanksgiving sides were just as dazzling as the centerpiece turkey? Imagi...",
+        votes: 0,
+        topic: "cooking",
+        author: "jessjelly",
+        created_at: "2018-05-06T02:40:35.489Z",
+        comment_count: "8"
+      }
+    ],
+    articlesByTopic: [
+      {
+        article_id: 33,
+        title: "Seafood substitutions are increasing",
+        body:
+          "'SEAFOOD fraud is a serious global problem', begins a recent report from Oceana, an NGO. Reviewin...",
+        votes: 0,
+        topic: "cooking",
+        author: "weegembump",
+        created_at: "2018-05-30T15:59:13.341Z",
+        comment_count: "6"
+      },
+      {
+        article_id: 28,
+        title: "High Altitude Cooking",
+        body:
+          "Most backpacking trails vary only a few thousand feet elevation. However, many trails can be foun...",
+        votes: 0,
+        topic: "cooking",
+        author: "happyamy2016",
+        created_at: "2018-05-27T03:32:28.514Z",
+        comment_count: "5"
+      },
+      {
+        article_id: 30,
+        title:
+          "Twice-Baked Butternut Squash Is the Thanksgiving Side Dish of Your Dreams",
+        body:
+          "What if, for once, your Thanksgiving sides were just as dazzling as the centerpiece turkey? Imagi...",
+        votes: 0,
+        topic: "cooking",
+        author: "jessjelly",
+        created_at: "2018-05-06T02:40:35.489Z",
+        comment_count: "8"
+      }
+    ],
+    isLoaded: true
+  };
   render() {
+    const { articleData, articlesByTopic, isLoaded } = this.state;
     return (
       <div>
         <h2>
           {!this.props.topic ? "hello from all articles" : "hello from "}
           {this.props.topic}
         </h2>
+        {isLoaded && (
+          <>
+            {!this.props.topic ? (
+              <ArticleList articleData={articleData} />
+            ) : (
+              <ArticleList articleData={articlesByTopic} />
+            )}
+          </>
+        )}
       </div>
     );
   }
