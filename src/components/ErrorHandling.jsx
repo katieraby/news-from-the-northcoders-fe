785cbc68b49c7b135bef7b530698b17c25927e7b
Katie Raby <katie.raby@icloud.com>
2020-03-19 11:21:15 +0000

    Edits to error handling component

--- src/components/ErrorHandling.jsx
+++ src/components/ErrorHandling.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ErrorHandling = ({ status, msg }) => {
-  return <div>Ooops error!</div>;
+  return (
+    <div>
+      <h2>Ooops error {status}!</h2>
+      <p>{msg}</p>
+    </div>
+  );
 };
 
 export default ErrorHandling;
