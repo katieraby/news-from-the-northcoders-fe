74bc527233265ed8fa038203893a2d22bd1d6669
Katie Raby <katie.raby@icloud.com>
2020-03-19 11:20:04 +0000

    Added error handling to comments list

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -3,19 +3,24 @@ import PostComment from "./PostComment";
 import styles from "./CommentList.module.css";
 import CommentCard from "./CommentCard";
 import * as api from "../API";
+import ErrorHandling from "../components/ErrorHandling";
 
 class CommentList extends Component {
   state = {
     commentData: [],
-    isLoaded: false
+    isLoaded: false,
+    err: null
   };
 
   render() {
-    const { commentData, isLoaded } = this.state;
+    const { commentData, isLoaded, err } = this.state;
     const { loggedInUser } = this.props;
     return (
       <main className={styles.listContainer}>
         <h3>Comments</h3>
+        {err !== null ? (
+          <ErrorHandling msg={err.msg} status={err.status} />
+        ) : null}
         {isLoaded ? (
           <>
             <p>{commentData.length} comments</p>
@@ -47,20 +52,32 @@ class CommentList extends Component {
     api.postComment(objToPost, this.props.article_id).then(({ data }) => {
       this.setState(prevState => {
         return { commentData: [data.comment, ...prevState.commentData] };
+      }).catch(err => {
+        this.setState({ err });
       });
     });
   };
 
   handleDelete = id => {
-    api.deleteComment(id).then(() => {
-      this.fetchCommentsByArticleId();
-    });
+    api
+      .deleteComment(id)
+      .then(() => {
+        this.fetchCommentsByArticleId();
+      })
+      .catch(err => {
+        this.setState({ err });
+      });
   };
 
   fetchCommentsByArticleId = () => {
-    api.fetchCommentsByArticleId(this.props.article_id).then(({ data }) => {
-      this.setState({ commentData: data.comments, isLoaded: true });
-    });
+    api
+      .fetchCommentsByArticleId(this.props.article_id)
+      .then(({ data }) => {
+        this.setState({ commentData: data.comments, isLoaded: true });
+      })
+      .catch(err => {
+        this.setState({ err });
+      });
   };
 
   componentDidMount() {
